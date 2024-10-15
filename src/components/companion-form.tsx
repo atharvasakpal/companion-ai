8c2b3a15cacc018224a4050ff57ff0305999cdce
Atharva Sakpal <atharvasakpal14@gmail.com>
2024-10-15 23:39:40 +0530

    companion-form done

--- src/components/companion-form.tsx
+++ src/components/companion-form.tsx
@@ -8,6 +8,7 @@ import { useForm } from 'react-hook-form'
 
 //shadcn imports
 import { Button } from "@/components/ui/button"
+
 import {
   Form,
   FormControl,
@@ -17,8 +18,21 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form"
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
+
 import { Separator } from './ui/separator'
 import ImageUpload from './image-upload'
+import { Input } from './ui/input'
+import { Textarea } from './ui/textarea'
+import { Wand2 } from 'lucide-react'
 
 interface CompanionFormProps{
     initialData  : Companion | null,
@@ -32,11 +46,11 @@ const formSchema = z.object({
     description: z.string().min(1, {
         message: "Description is required"
      }),
-    instructions: z.string().min(300, {
-        message: "Atleast 200 characters required"
+    instructions: z.string().min(100, {
+        message: "Atleast 100 characters required"
      }),
-    seed: z.string().min(300, {
-        message: "Atleast 200 characters required"
+    seed: z.string().refine(val => val.length === 0 || val.length >= 100, {
+    message: "At least 100 characters required if not empty"
     }),
     src: z.string().min(1, {
         message: "Image is required"
@@ -96,6 +110,132 @@ const CompanionForm = ({initialData, categories}: CompanionFormProps) => {
                         </FormItem>
                     )}
                     />
+                <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+                    <FormField
+                        control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                        <FormItem className='col-span-2 md:col-span-1'>
+
+                            <FormLabel>Name</FormLabel>
+                            <FormControl>
+                            <Input disabled={isLoading} placeholder='Elon Musk' {...field} />
+                            </FormControl>
+                             <FormDescription>
+                                This is how your AI companion will be named
+                            </FormDescription>
+                            <FormMessage />
+                            
+                        </FormItem>
+                    )}
+                    />
+
+
+                    <FormField
+                        control={form.control}
+                    name="description"
+                    render={({ field }) => (
+                        <FormItem className='col-span-2 md:col-span-1'>
+
+                            <FormLabel>Description</FormLabel>
+                            <FormControl>
+                            <Input disabled={isLoading} placeholder='CEO & founder of Tesla , SpaceX' {...field} />
+                            </FormControl>
+                            <FormDescription>
+                                Short Description of your AI companion
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                    />
+
+
+                    <FormField
+                        control={form.control}
+                    name="categoryId"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Category</FormLabel>
+                            <Select disabled={isLoading} onValueChange={field.onChange} value={field.value} defaultValue={field.value}>
+                                <FormControl>
+                                <SelectTrigger className="bg-background">
+                                    <SelectValue placeholder="Select a category" />
+                                </SelectTrigger>
+                                </FormControl>
+
+                                <SelectContent>
+                                    {categories.map((item)=>(
+                                         <SelectItem key={item.id} value={item.id}>{item.name}</SelectItem>
+                                    ))}
+                                   
+                                    
+                                </SelectContent>
+                            </Select>
+
+
+                            <FormDescription>
+                                Select a category for your AI
+                            </FormDescription>
+                                    <FormMessage />
+                        </FormItem>
+                    )}
+                    />
+
+                </div>
+                <div className='space-y-2 w-full'>
+                    <div>
+                        <h3 className='text-lg font-medium'>
+                            Configuration
+                        </h3>
+                        <p className='text-sm text-muted-foreground'>
+                            Detailed instructions for AI behaviour
+                        </p>
+                    </div>
+                    <Separator />
+                </div>
+                        <FormField
+                        control={form.control}
+                    name="instructions"
+                    render={({ field }) => (
+                        <FormItem className='col-span-2 md:col-span-1'>
+
+                            <FormLabel>Instructions</FormLabel>
+                            <FormControl>
+                            <Textarea className='bg-background resize-none' rows={7} disabled={isLoading} placeholder='You are Elon Musk, the visionary entrepreneur and CEO of Tesla and SpaceX. You speak with a mix of casual confidence and intellectual depth, often diving into topics like space exploration, AI, clean energy, and innovation. You’re known for your optimistic view of the future, bold predictions, and sometimes playful or quirky sense of humor. You like to think outside the box and aren’t afraid of tackling hard challenges. While you may drop a joke or meme reference now and then, your thoughts are always focused on solving big problems. Respond with brevity but clarity, using a direct, no-nonsense style.' {...field} />
+                            </FormControl>
+                            <FormDescription>
+                                Describe your companion's backstory and relevant details
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                    />
+
+
+
+                    <FormField
+                        control={form.control}
+                    name="seed"
+                    render={({ field }) => (
+                        <FormItem className='col-span-2 md:col-span-1'>
+
+                            <FormLabel>Example conversation</FormLabel>
+                            <FormControl>
+                            <Textarea className='bg-background resize-none' rows={7} disabled={isLoading} placeholder='Human: Hey Elon, what do you think is the next big leap in space exploration? Will we see humans on Mars soon?
+Elon Musk: Yeah, absolutely. I’d say getting humans to Mars is the big one. It’s the next logical step for making life multi-planetary. SpaceX is making a lot of progress with Starship. If things go well, I think we could see an uncrewed mission to Mars in the next 3-4 years, maybe a crewed one shortly after.' />
+                            </FormControl>
+                            <FormDescription>
+                               Write a seed conversation between a Human and your companion
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                    />
+
+
+                    <div className='w-full flex justify-center'>
+                        <Button size='lg' disabled={isLoading}>{initialData? 'Edit Your Companion' : 'Create Your Companion'}<Wand2 className='ml-2'/></Button>
+                    </div>
             </form>
         </Form>
       
